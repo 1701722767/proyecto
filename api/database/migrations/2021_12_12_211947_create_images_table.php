cb96643ce8fa7965024d62045dd04fed53e1ff5d
andresfelipe11 <andres.1701714898@gmail.com>
2021-12-12 16:41:34 -0500

    Model_creation

---
+++ api/database/migrations/2021_12_12_211947_create_images_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_file');
+            $table->unsignedBigInteger('site_id');
+            $table->foreign('site_id')->references('id')
+                ->on('sites')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
