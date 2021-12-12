d2053dc100f8f47eef563226b356cd2e98e054b0
santiago-ocampo8 <51138167+santiago-ocampo8@users.noreply.github.com>
2021-12-12 13:41:21 -0500

    Correction tests

--- api/tests/Unit/SendEmailTest.php
+++ api/tests/Unit/SendEmailTest.php
@@ -2,12 +2,14 @@
 
 namespace Tests\Feature;
 
+namespace Tests\Feature;
+
 use App\Http\Controllers\MailController;
-use Illuminate\Http\Request;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendEmailMailable;
-
+use Illuminate\Support\Facades\Log;
+use TiMacDonald\Log\LogFake;
 class SendEmailTest extends TestCase
 {
     /**
@@ -17,20 +19,22 @@ class SendEmailTest extends TestCase
      */
     public function test_send_email()
     {
+        Log::swap(new LogFake);
+
         Mail::fake();
         $temporal = new MailController();
-        $response = $temporal->getEMail('santiago.1701721006@ucaldas.edu.co','This is a Subject','This is a Message');
-        $this->assertEquals($response->error, false);
-        Mail::assertSent(SendEmailMailable::class);
+        $temporal->sendEmail('santiago.1701721006@ucaldas.edu.co', 'This is a Subject', 'This is a Message');
 
+        Mail::assertSent(SendEmailMailable::class);
     }
 
     public function test_send_email_failed()
     {
+        Log::swap(new LogFake);
         Mail::fake();
         $temporal = new MailController();
-        $response = $temporal->getEMail('santiago.1701721006ucaldas.edu.co','This is a Subject','This is a Message');    
-        $this->assertEquals($response->error, true);
+        $this->expectErrorMessage('El correo tiene que ser una dirección de correo válido');
+        $temporal->sendEmail('santiago.1701721006ucaldas.edu.co', 'This is a Subject', 'This is a Message');
         Mail::assertNotSent(SendEmailMailable::class);
     }
 }
