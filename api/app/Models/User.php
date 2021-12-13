8bb44ccb1f25b53e7da2f178ca5ed442923e482f
Juan Giraldo <juacagiri@gmail.com>
2021-12-13 11:03:09 -0500

    Fix the logger for get user information
    - Get user information
    - Add log when fail the emailNotifier

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
